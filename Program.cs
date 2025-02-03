bb607eeb39331160976412f3992e92585bba4ff3
Name <vbrfqlj@mail.ru>
2025-02-03 23:02:07 +0300

    Задание 4.3.13: Сумма всех чисел

--- Program.cs
+++ Program.cs
@@ -6,24 +6,14 @@ class MainClass
     {
         var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-        int k;
-
-        for (int i = 0; i < arr.Length; i++)
-        {
-            for (int j = i + 1; j < arr.Length; j++)
-            {
-                if (arr[i] > arr[j])
-                {
-                    k = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = k;
-                }
-            }
-        }
+        int sum = 0;
 
         foreach (int i in arr)
         {
-            Console.WriteLine(i + " ");
+            sum += i;
         }
+
+        Console.WriteLine(sum);
+
     }
 }
