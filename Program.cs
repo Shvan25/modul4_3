4b5b5cb048fbe6be19473a390e24c6e171348f4f
Name <vbrfqlj@mail.ru>
2025-02-03 21:54:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 6, 30, 7, 14, 2, 15 };
+            int minValue = 100000,
+            minIndex = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < minValue)
+                {
+                    minValue = numbers[i];
+                    minIndex = i;
+                }
+            }
+
+            Console.WriteLine("Наименьший элемент массива имеет индекс {0}, значение этого элемента = {1}", minIndex, numbers[minIndex]);
+
+            Console.ReadKey();
+        }
+    }
+}
