218afa13da5885b0832078fd5fdc04407df6e0aa
Name <vbrfqlj@mail.ru>
2025-02-03 23:04:38 +0300

    Задание 4.3.14: перебор массива

--- Program.cs
+++ Program.cs
@@ -4,16 +4,18 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+        int[][] array = new int[3][];
+        array[0] = new int[2] { 1, 2 };
+        array[1] = new int[3] { 1, 2, 3 };
+        array[2] = new int[5] { 1, 2, 3, 4, 5 };
 
-        int sum = 0;
-
-        foreach (int i in arr)
+        foreach (var num in array)
         {
-            sum += i;
+            foreach (var item in num)
+            {
+                Console.Write(item + " ");
+            }
         }
-
-        Console.WriteLine(sum);
 
     }
 }
