d841f659e3e18563d793090af8bbe2f1627bcf4f
Name <vbrfqlj@mail.ru>
2025-02-03 22:59:35 +0300

    Задание 4.3.12: сортировка

--- Program.cs
+++ Program.cs
@@ -4,14 +4,26 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+        var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-        for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+        int k;
+
+        for (int i = 0; i < arr.Length; i++)
         {
-            for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
-                Console.Write(array[k, i] + " ");
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    k = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = k;
+                }
+            }
+        }
 
-            Console.WriteLine();
+        foreach (int i in arr)
+        {
+            Console.WriteLine(i + " ");
         }
     }
 }
